7ccdbaf8576e32693ca50a1baa5fcd973b159e26
Nhan Vo <vodainhan.080504@gmail.com>
2025-10-08 08:03:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3896797610-76c8c",
+  "appId": "1:877973715548:web:ae970b7e6d679372ae0499",
+  "apiKey": "AIzaSyC-ipRYNd3lQRdlQ3f85H-v3xtETbQeRfA",
+  "authDomain": "studio-3896797610-76c8c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "877973715548"
+};
